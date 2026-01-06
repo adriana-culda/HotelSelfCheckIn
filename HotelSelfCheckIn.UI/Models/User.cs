74ac11a70ed5701ea802b50f27712d68b60b4d32
Lara <nklarissa.m@gmail.com>
2026-01-06 21:16:22 +0200

    implementare access level pentru utilizare MANAGER

--- HotelSelfCheckIn.UI/Models/User.cs
+++ HotelSelfCheckIn.UI/Models/User.cs
@@ -4,10 +4,12 @@ public abstract record User
 {
     public string Username { get; init; }
     public string Password { get; init; }
+    public string AccessLevel { get; init; }
 
-    protected User(string username, string password)
+    protected User(string username, string password, string accessLevel)
     {
         Username = username;
         Password = password;
+        AccessLevel = accessLevel;
     }
 }
