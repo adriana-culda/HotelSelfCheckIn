9e659bf494f14164a488d41704cf4269da0228a1
Adriana Culda <culdaadriana01@gmail.com>
2026-01-11 23:23:06 +0200

    Am creat ClientShellView si am inceput sa implementez cateva parti din pagina

---
+++ HotelSelfCheckIn.UI/Views/CCheckInOutView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace HotelSelfCheckIn.UI.Views;
+
+public partial class CCheckInOutView : UserControl
+{
+    public CCheckInOutView()
+    {
+        InitializeComponent();
+    }
+}
